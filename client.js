e5427b4f7cb457a5fe6cea1db07d96b64611e4c2
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-01 16:48:34 -0500

    add command

--- client.js
+++ client.js
@@ -1,12 +1,34 @@
-const net = require('net');
-// const connect = ()=>{
-  const client = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: 50541
+const net = require("net");
+const connect = () => {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541,
   });
 
   // interpret incoming data as text
-  client.setEncoding("utf8");
-// return conn;
-// };
-module.exports = client;
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    let time = 1000;
+    let count = 5;
+
+    console.log("Successfully connected to game server.");
+    conn.write(`Name: JUB`);
+    for (let i = 0; i < 10; i++) {
+      setTimeout(() => {
+        conn.write(`Move: up`);
+      }, (time += 200));
+      setTimeout(() => {
+        conn.write(`Move: left`);
+      }, (time += 200));
+      setTimeout(() => {
+        conn.write(`Move: up`);
+      }, (time += 200));
+      setTimeout(() => {
+        conn.write(`Move: left`);
+      }, (time += 200));
+    }
+    return conn;
+  });
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,18 +1,14 @@
 const net = require("net");
-const client = require('./client');
+const connect = require('./client');
 
 // establishes a connection with the game server
+    console.log("Connecting ...");
+    connect();
+  
 
-console.log("Connecting ...");
-//connect();
-client.on('connect',()=>{
-  console.log('client is connected to server.');
-});
-client.on('data',(message)=>{
-  console.log('server sent:',message);
-})
-client.on('end', function(){
-  console.log('client disconnected from server');
-});
-
-
+// client.on('data',(message)=>{
+//   console.log('server sent:',message);
+// })
+// client.on('end', function(){
+//   console.log('client disconnected from server');
+// });

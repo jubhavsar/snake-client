bc18a9c67a604601ecf5b3a1afd28b54346ef222
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-01 18:09:27 -0500

    refactoring

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const net = require("net");
-const connect = require('./client');
-const stdin = process.stdin;
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  //stdin.write('');
-};
+const  connect  = require('./client');
+const  setupInput  = require('./input');
 
 // establishes a connection with the game server
     console.log("Connecting ...");
